23f0878c5d044ec7910a92ae74bdd40129aab59a
Ítalo Andrade <andraditalo@gmail.com>
2018-09-13 20:54:46 -0300

    Check if user has a session when the page loads faster than the API

--- src/app/utils/auth.guard.ts
+++ src/app/utils/auth.guard.ts
@@ -2,21 +2,45 @@ import {Injectable} from '@angular/core';
 import {Router, CanActivate} from '@angular/router';
 import {UserService} from './user.service';
 import {MatSnackBar} from '@angular/material';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private userService: UserService, private router: Router, private snackBar: MatSnackBar) {
   }
 
-  canActivate(): boolean {
-    if (!this.userService.user) {
-      this.router.navigate(['sign-in']);
-      this.snackBar.open('Para acessar essa página você tem que iniciar a sessão', null, {
-        duration: 3000
+  canActivate(): any {
+    if (typeof this.userService.user === 'undefined') {
+      return new Observable((observer) => {
+        const timeout = setTimeout(() => {
+          observer.next(false);
+          this.router.navigate(['/']);
+          this.snackBar.open('Tempo para verificar sua sessão esgotou', null, {
+            duration: 3000
+          });
+        }, 60000);
+
+        this.userService.userChange.subscribe(user => {
+          clearTimeout(timeout);
+          observer.next(!!user);
+          if (!user) {
+            this.router.navigate(['sign-in']);
+            this.snackBar.open('Para acessar essa página você tem que iniciar a sessão', null, {
+              duration: 3000
+            });
+          }
+        });
       });
-      return false;
+    } else {
+      if (!this.userService.user) {
+        this.router.navigate(['sign-in']);
+        this.snackBar.open('Para acessar essa página você tem que iniciar a sessão', null, {
+          duration: 3000
+        });
+        return false;
+      }
+      return true;
     }
-    return true;
   }
 }
 
