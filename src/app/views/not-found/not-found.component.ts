f108c2feb58f4cdc25b3af2ce4f19f4b6e90edec
Ítalo Andrade <andraditalo@gmail.com>
2018-09-14 00:25:17 -0300

    A better not-found page (but still not the definite one)

--- src/app/views/not-found/not-found.component.ts
+++ src/app/views/not-found/not-found.component.ts
@@ -3,7 +3,8 @@ import {AppComponent} from '../../app.component';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: 'not-found.component.html'
+  templateUrl: 'not-found.component.html',
+  styleUrls: ['not-found.component.scss']
 })
 
 export class NotFoundComponent implements OnInit {
