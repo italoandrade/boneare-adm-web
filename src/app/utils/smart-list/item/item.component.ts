87e6d202a360304ff345d4af06efd4c3d7e8fa3f
Ítalo Andrade <andraditalo@gmail.com>
2018-09-21 01:18:13 -0300

    Smart list

---
+++ src/app/utils/smart-list/item/item.component.ts
@@ -0,0 +1,47 @@
+import {Component, ElementRef, Inject, AfterViewInit} from '@angular/core';
+import {SmartListComponent} from '../smart-list.component';
+import {elementClosest} from '../../element/closest.function';
+
+@Component({
+  selector: 'smart-list-item',
+  templateUrl: '../smart-list.component.html'
+})
+
+export class SmartListItemComponent implements AfterViewInit {
+  constructor(@Inject(SmartListComponent) private parent: SmartListComponent, private element: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    const self = this;
+    const inputs = this.element.nativeElement.querySelectorAll('input');
+    for (let i = 0; i < inputs.length; i++) {
+      const el = inputs[i];
+      el.addEventListener('focus', e => {
+        this.onFocus(e, self);
+      });
+
+      el.addEventListener('blur', e => {
+        this.onBlur(e, self);
+      });
+    }
+  }
+
+  onFocus(event, self) {
+    const el = event.target;
+
+    const elItem = elementClosest(el, 'smart-list-item');
+    const newIndex = Array.prototype.indexOf.call(self.parent.element.nativeElement.children, elItem);
+    self.parent.currentFocusedElementIndex = newIndex > -1 ? newIndex : self.parent.currentFocusedElementIndex;
+  }
+
+  onBlur(event, self) {
+    setTimeout(() => {
+      const target = event.target;
+      if (!elementClosest(target, 'body')) {
+        setTimeout(() => {
+          self.parent.element.nativeElement.children[self.parent.currentFocusedElementIndex].querySelector('input').focus();
+        }, 100);
+      }
+    });
+  }
+}
