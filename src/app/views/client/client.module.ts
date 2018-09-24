085bd593f4d2cf8d85cb89b6faffb9141e471312
Ítalo Andrade <andraditalo@gmail.com>
2018-09-24 17:47:40 -0300

    Minor fixes

--- src/app/views/client/client.module.ts
+++ src/app/views/client/client.module.ts
@@ -1,42 +1,33 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID, NgModule} from '@angular/core';
 import {ClientListComponent} from './list/client-list.component';
-import {MaterialComponentsModule} from '../../material-components.module';
+import {ComponentsModule} from '../../components.module';
 import {RouterModule} from '@angular/router';
 import {MatPaginatorIntl} from '@angular/material';
 import {MatPaginatorIntlBra} from '../../utils/matPaginatorIntlBra';
 import {BrowserModule} from '@angular/platform-browser';
 import {ClientListService} from './list/client-list.service';
 import {ClientInfoComponent} from './info/client-info.component';
-import {DisplacerComponent, DisplacerPortalDirective} from '../../utils/displacer';
 import {FormsModule} from '@angular/forms';
 import {registerLocaleData} from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import {TextareaAutosizeModule} from 'ngx-textarea-autosize';
-import {MaskPhoneDirective} from '../../utils/masks/mask-phone.directive';
-import {SmartListComponent} from '../../utils/smart-list/smart-list.component';
-import {SmartListItemComponent} from '../../utils/smart-list/item/item.component';
 
 registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
     ClientListComponent,
-    ClientInfoComponent,
-
-    DisplacerComponent,
-    DisplacerPortalDirective,
-    MaskPhoneDirective,
-    SmartListComponent,
-    SmartListItemComponent
+    ClientInfoComponent
   ],
   imports: [
     BrowserModule,
     RouterModule,
     FormsModule,
-    MaterialComponentsModule,
+    ComponentsModule,
     TextareaAutosizeModule
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}, {provide: MatPaginatorIntl, useClass: MatPaginatorIntlBra}, ClientListService]
+  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}, {provide: MatPaginatorIntl, useClass: MatPaginatorIntlBra}, ClientListService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ClientModule {
 }
