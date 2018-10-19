797a326546fed3a473efbd90fcc3bd2c3242735a
Ítalo Andrade <andraditalo@gmail.com>
2018-10-18 23:58:43 -0300

    Design simle dashboard

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -8,7 +8,7 @@ import {AppComponent} from '../../app.component';
 })
 
 export class HomeComponent implements OnInit {
-  constructor(private appComponent: AppComponent) {
+  constructor(public appComponent: AppComponent) {
   }
 
   ngOnInit() {
